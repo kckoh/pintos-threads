6a8b75363e2af64c4cf830beea81eb219bf4ddc1
uyeong <uyeong1575@gmail.com>
2025-11-13 11:20:46 +0900

    advance 개선

--- pintos/threads/thread.c
+++ pintos/threads/thread.c
@@ -203,17 +203,16 @@ tid_t thread_create(const char *name, int priority,
 	if (t == NULL)
 		return TID_ERROR;
 
-	/* Initialize thread. */
-	init_thread(t, name, priority);
-	tid = t->tid = allocate_tid();
-
 	struct thread *parent = thread_current();
 	if (t != idle_thread)
 	{
 		t->nice = parent->nice;
 		t->recent_cpu = parent->recent_cpu;
-		t->priority = calc_priority(t);
 	}
+
+	/* Initialize thread. */
+	init_thread(t, name, priority);
+	tid = t->tid = allocate_tid();
 
 	/* Call the kernel_thread if it scheduled.
 	 * Note) rdi is 1st argument, and rsi is 2nd argument. */
