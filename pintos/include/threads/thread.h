ab29c89a302c41e85f6b57836ca36bac5ad11e40
Yuntaeeun <banya91201@naver.com>
2025-11-28 19:57:07 +0900

    format

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -14,10 +14,10 @@
 /* States in a thread's life cycle. */
 enum thread_status
 {
-	THREAD_RUNNING, /* Running thread. */
-	THREAD_READY,	/* Not running but ready to run. */
-	THREAD_BLOCKED, /* Waiting for an event to trigger. */
-	THREAD_DYING	/* About to be destroyed. */
+    THREAD_RUNNING, /* Running thread. */
+    THREAD_READY,   /* Not running but ready to run. */
+    THREAD_BLOCKED, /* Waiting for an event to trigger. */
+    THREAD_DYING    /* About to be destroyed. */
 };
 
 /* Thread identifier type.
@@ -26,9 +26,9 @@ typedef int tid_t;
 #define TID_ERROR ((tid_t) - 1) /* Error value for tid_t. */
 
 /* Thread priorities. */
-#define PRI_MIN 0	   /* Lowest priority. */
+#define PRI_MIN 0      /* Lowest priority. */
 #define PRI_DEFAULT 31 /* Default priority. */
-#define PRI_MAX 63	   /* Highest priority. */
+#define PRI_MAX 63     /* Highest priority. */
 #define FD_TABLE_SIZE 128
 
 /* A kernel thread or user process.
@@ -90,48 +90,49 @@ typedef int tid_t;
  * blocked state is on a semaphore wait list. */
 struct thread
 {
-	/* Owned by thread.c. */
-	tid_t tid;				   /* Thread identifier. */
-	enum thread_status status; /* Thread state. */
-	char name[16];			   /* Name (for debugging purposes). */
-	int priority;			   /* Priority(donate 시 바뀜) */
-	int original_priority;	   /* 원래 priority*/
-	int64_t wake_tick;		   /* Time to wake up (for timer_sleep) */
-	/* Shared between thread.c and synch.c. */
-	struct list_elem elem;		  /* List element. */
-	struct list_elem donate_elem; /* 내가 donate하는 경우 주는 elem */
-	struct list donaters;		  /* 나에게 donate한 thread 확인 */
-	struct lock *waiting_lock;	  /* 내가 기다리는 lock(release시 확인용)*/
+    /* Owned by thread.c. */
+    tid_t tid;                 /* Thread identifier. */
+    enum thread_status status; /* Thread state. */
+    char name[16];             /* Name (for debugging purposes). */
+    int priority;              /* Priority(donate 시 바뀜) */
+    int original_priority;     /* 원래 priority*/
+    int64_t wake_tick;         /* Time to wake up (for timer_sleep) */
+    /* Shared between thread.c and synch.c. */
+    struct list_elem elem;        /* List element. */
+    struct list_elem donate_elem; /* 내가 donate하는 경우 주는 elem */
+    struct list donaters;         /* 나에게 donate한 thread 확인 */
+    struct lock* waiting_lock;    /* 내가 기다리는 lock(release시 확인용)*/
 #ifdef USERPROG
-	/* Owned by userprog/process.c. */
-	uint64_t *pml4; /* Page map level 4 */
+    /* Owned by userprog/process.c. */
+    uint64_t* pml4; /* Page map level 4 */
 
-	int exit_status;
-	struct file **fd_table;
-	int fd_capacity;
+    int exit_status;
+    struct file** fd_table;
+    int fd_capacity;
 
-	struct list child_list;
-	// struct thread *parent;
-	struct child *child_info;
-	struct file *executable;
+    struct list child_list;
+    // struct thread *parent;
+    struct child* child_info;
+    struct file* executable;
 
 #endif
 #ifdef VM
-	/* Table for whole virtual memory owned by thread. */
-	struct supplemental_page_table spt;
+    /* Table for whole virtual memory owned by thread. */
+    struct supplemental_page_table spt;
 #endif
 
-	/* Owned by thread.c. */
-	struct intr_frame tf; /* Information for switching */
-	unsigned magic;		  /* Detects stack overflow. */
+    /* Owned by thread.c. */
+    struct intr_frame tf; /* Information for switching */
+    unsigned magic;       /* Detects stack overflow. */
 };
 
-struct child {
-	struct list_elem child_elem;
-	tid_t child_tid;
-	int exit_status;
-	bool waited;
-	struct semaphore wait_sema;
+struct child
+{
+    struct list_elem child_elem;
+    tid_t child_tid;
+    int exit_status;
+    bool waited;
+    struct semaphore wait_sema;
 };
 
 /* If false (default), use round-robin scheduler.
@@ -145,15 +146,15 @@ void thread_start(void);
 void thread_tick(void);
 void thread_print_stats(void);
 
-typedef void thread_func(void *aux);
-tid_t thread_create(const char *name, int priority, thread_func *, void *);
+typedef void thread_func(void* aux);
+tid_t thread_create(const char* name, int priority, thread_func*, void*);
 
 void thread_block(void);
-void thread_unblock(struct thread *);
+void thread_unblock(struct thread*);
 
-struct thread *thread_current(void);
+struct thread* thread_current(void);
 tid_t thread_tid(void);
-const char *thread_name(void);
+const char* thread_name(void);
 
 void thread_exit(void) NO_RETURN;
 void thread_yield(void);
@@ -166,16 +167,16 @@ void thread_set_nice(int);
 int thread_get_recent_cpu(void);
 int thread_get_load_avg(void);
 
-void do_iret(struct intr_frame *tf);
+void do_iret(struct intr_frame* tf);
 
 // thread clock alarm
 void thread_sleep(int64_t wake_tick);
 void thread_wake_sleeping(int64_t current_tick);
-bool sleep_less(struct list_elem *a, struct list_elem *b, void *aux UNUSED);
-bool priority_more(struct list_elem *a, struct list_elem *b, void *aux UNUSED);
-bool priority_less(struct list_elem *a, struct list_elem *b, void *aux UNUSED);
-bool cond_priority_less(struct list_elem *a, struct list_elem *b, void *aux UNUSED);
-bool donate_less(struct list_elem *a, struct list_elem *b, void *aux UNUSED);
+bool sleep_less(struct list_elem* a, struct list_elem* b, void* aux UNUSED);
+bool priority_more(struct list_elem* a, struct list_elem* b, void* aux UNUSED);
+bool priority_less(struct list_elem* a, struct list_elem* b, void* aux UNUSED);
+bool cond_priority_less(struct list_elem* a, struct list_elem* b, void* aux UNUSED);
+bool donate_less(struct list_elem* a, struct list_elem* b, void* aux UNUSED);
 void check_donate_priority(void);
 
 // thread clock alarm
